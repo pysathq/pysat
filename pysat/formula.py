a2ba3d0d44da1d68fea1f16bb4c6f4e21cb5acb6
Marcell Vazquez-Chanlatte <mvc@linux.com>
2019-10-29 10:24:47 -0700

    Make sure literals are positive when taking max

--- pysat/formula.py
+++ pysat/formula.py
@@ -610,7 +610,7 @@ class CNF(object):
         
         self.clauses = [list(cls) for cls in aig_cnf.clauses]
         self.comments = ['c ' + c.strip() for c in aig_cnf.comments]
-        self.nv = max(itertools.chain(*self.clauses))
+        self.nv = max(map(abs, itertools.chain(*self.clauses)))
 
         # saving input and output variables
         self.inps = list(aig_cnf.input2lit.values())
