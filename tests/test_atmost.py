f919f734081dab0a7c99c81c6e062cd5192b4d53
Alexey Ignatiev <alexey.ignatiev@monash.edu>
2020-08-03 21:29:08 +1000

    Fixed the issue of wrong top id when empty cardinality encoding is created.

---
+++ tests/test_atmost.py
@@ -0,0 +1,20 @@
+from pysat.formula import IDPool
+from pysat.card import CardEnc
+
+vp = IDPool()
+n = 20
+b = 50
+assert n <= b
+
+lits = [vp.id(v) for v in range(1, n + 1)]
+top = vp.top
+
+G = CardEnc.atmost(lits, b, vpool=vp)
+
+assert len(G.clauses) == 0
+
+try:
+    assert vp.top >= top
+except AssertionError as e:
+    print(f"\nvp.top = {vp.top} (expected >= {top})\n")
+    raise e
