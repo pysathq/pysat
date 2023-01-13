f554db418bec79693746657857be49ab733f7fab
Alexey Ignatiev <alexey.ignatiev@monash.edu>
2023-01-13 20:53:29 +1100

    Added warm start testing.

---
+++ tests/test_warmstart.py
@@ -0,0 +1,17 @@
+from pysat.card import CardEnc
+from pysat.solvers import Solver
+
+def test_warmstart():
+    n, b = 5, 3
+
+    cnf = CardEnc.atmost(range(1, n + 1), b)
+
+    for name in ['m22', 'mgh', 'mpl', 'mc', 'g30', 'g41', 'gc30', 'gc41']:
+        models1 = []
+        with Solver(name=name, bootstrap_with=cnf, warm_start=False) as solver:
+            models1 = [model for model in solver.enum_models()]
+
+        with Solver(name=name, bootstrap_with=cnf, warm_start=True) as solver:
+            models2 = [model for model in solver.enum_models()]
+
+        assert sorted(models1) == sorted(models1), 'Wrong set/number of models for {0}'.format(name)
