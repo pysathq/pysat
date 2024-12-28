36b2e1d85ad39ab092332ede7b68e19d5d1dabc1
Gavin Peters <gavin@ytz.ca>
2024-12-28 16:39:23 -0500

    Add simple tests of CNF behaviour (construction, copy)
    Copying a new empty CNF should not raise an exception.

--- tests/test_cnf.py
+++ tests/test_cnf.py
@@ -27,6 +27,16 @@ cnf1.append(range(1, 6))  # at least 1
 cnf2 = CardEnc.atmost(lits=range(1, 6), bound=1, encoding=EncType.seqcounter)
 cnf2.append(range(1, 6))  # at least 1
 
+def test_cnf():
+    cnf = CNF()
+    cnf_copy = cnf.copy()
+    # TODO: check actual values of the copy for sanity.
+
+def test_cnfplus():
+    cnfplus = CNFPlus()
+    cnfplus_copy = cnfplus.copy()
+    # TODO: check actual values of the copy for sanity.
+
 def test_cnfplus_solvers():
     # testing cnf1:
     for name in solvers:
