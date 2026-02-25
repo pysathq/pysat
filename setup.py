ddc3c8f9c532e0af791f3dc27e87c61832aa9cc8
Alexey Ignatiev <alexey.ignatiev@monash.edu>
2026-02-25 12:42:14 +1100

    Minor rearranging.

--- setup.py
+++ setup.py
@@ -70,8 +70,8 @@ Details can be found at `https://pysathq.github.io <https://pysathq.github.io>`_
 #==============================================================================
 to_install = ['cadical103', 'cadical153', 'cadical195', 'gluecard30',
               'gluecard41', 'glucose30', 'glucose41', 'glucose421',
-              'lingeling', 'maplechrono', 'maplecm', 'maplesat', 'mergesat3',
-              'minicard', 'minisat22', 'minisatgh', 'kissat404']
+              'kissat404', 'lingeling', 'maplechrono', 'maplecm', 'maplesat',
+              'mergesat3', 'minicard', 'minisat22', 'minisatgh']
 
 # example and allies scripts to install as standalone executables
 #==============================================================================
