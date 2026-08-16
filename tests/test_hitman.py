d109414b87e20ffd001c7ac797a714c8bb5acc51
Alexey Ignatiev <alexey.ignatiev@monash.edu>
2026-08-16 13:52:07 +1000

    A few more tests.

--- tests/test_hitman.py
+++ tests/test_hitman.py
@@ -1,11 +1,12 @@
 from pysat.examples.hitman import Atom, Hitman
 
 
-def test_add_hard_registers_new_object_with_lbx():
-    with Hitman(bootstrap_with=[['first']], htype='lbx') as hitman:
-        hitman.add_hard([Atom('second')])
+def test_add_hard_registers_new_object_with_mcs_enumerators():
+    for htype in ('lbx', 'mcsls'):
+        with Hitman(bootstrap_with=[['first']], htype=htype) as hitman:
+            hitman.add_hard([Atom('second')])
 
-        assert sorted(hitman.get()) == ['first', 'second']
+            assert sorted(hitman.get()) == ['first', 'second']
 
 
 def test_add_hard_registers_weighted_new_objects_with_rc2():
@@ -16,3 +17,15 @@ def test_add_hard_registers_weighted_new_objects_with_rc2():
         hitman.add_hard([Atom('expensive'), Atom('cheap')], weights=weights)
 
         assert sorted(hitman.get()) == ['base', 'cheap']
+
+
+def test_add_hard_registers_new_objects_with_sat():
+    with Hitman(bootstrap_with=[['base']], htype='sat',
+            solver='mgh') as hitman:
+        hitman.add_hard([Atom('left'), Atom('right')])
+
+        result = hitman.get()
+
+        assert 'base' in result
+        assert len(set(result) & {'left', 'right'}) == 1
+        assert set(hitman.oracle.vmap.e2i) == {1, 2, 3}
